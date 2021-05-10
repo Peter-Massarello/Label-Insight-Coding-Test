d8176c986b969517b98a0432ba1c38cdba35b04c
Peter-Massarello <pmassarello@gmail.com>
2021-05-10 10:30:22 -0700

    Created file for second problem

---
+++ src/ProblemTwo.java
@@ -0,0 +1,9 @@
+public class ProblemTwo {
+
+    public static void animate(int speed, String init) {}
+
+
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
