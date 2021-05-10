92895399dca917fbc10ade204b5400ee383a7a3c
Peter-Massarello <pmassarello@gmail.com>
2021-05-10 11:22:40 -0700

    Moving positions of single particles working

--- src/ProblemTwo.java
+++ src/ProblemTwo.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProblemTwo {
 
     public static class Particle {
@@ -11,8 +14,11 @@ public class ProblemTwo {
             this.position = position;
         }
 
-        public void movePosition(int newPosition) {
-            this.position = newPosition;
+        public void movePosition() {
+            if (this.direction == 'L')
+                this.position -= speed;
+            else if (this.direction == 'R')
+                this.position += speed;
         }
 
         public void printPosition(){
@@ -21,26 +27,33 @@ public class ProblemTwo {
 
     }
 
-    public static void animate(int speed, String init) {
+    public static String[] animate(int speed, String init) {
         char[] charArray = init.toCharArray();
-        Particle[] pArray = new Particle[init.length()];
-        int indexCounter = 0;
+        List<Particle> pArray = new ArrayList<Particle>();
+        String[] finalAnimation = {""};
 
         for (char c: charArray) {
             if (c == 'R' || c == 'L') {
                 System.out.println("Found particle");
-                pArray[indexCounter] = new Particle(c, speed, init.indexOf(c));
-                indexCounter++;
+                pArray.add(new Particle(c, speed, init.indexOf(c)));
             }
         }
 
+        for (int i = 0; i < 4; i++) {
+            for (Particle p: pArray) {
+                p.printPosition();
+                p.movePosition();
+            }
+        }
         for (Particle p: pArray) {
             p.printPosition();
         }
+
+        return finalAnimation;
     }
 
 
     public static void main(String[] args) {
-        animate(2, "..R.L.R.");
+        animate(2, "..R....");
     }
 }
