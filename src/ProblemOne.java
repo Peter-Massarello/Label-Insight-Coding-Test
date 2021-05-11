c336b48a38cebfbb603827d5960f5c2fcce26b59
Peter-Massarello <pmassarello@gmail.com>
2021-05-10 17:48:51 -0700

    Final build of both problems, maybe will do more touchups before submission time

--- src/ProblemOne.java
+++ src/ProblemOne.java
@@ -1,36 +1,34 @@
 public class ProblemOne {
 
-    // Main method
+    /* Getting Letters Method
+        - Declares two variables, a boolean to check if current character is a valid char
+          and a String that contains the whole alphabet which will be incrementally chipped off as program runs
+        - Takes input string and removes whitespace and sets to lowercase
+        - Sets new input String to char array for ease of looping
+        - Iterates through char array and if an alpha char is found AND the 'alph' String contains that character
+          then remove that char from the final string, since that letter has been found
+        - If the length of the 'alph' String reaches 0, break out of loop and return String. This is used for
+          early terminations where the input string is very long, but midway through the entire alphabet has been found
+     */
     public static String getMissingLetters(String input) {
-        // Initial variable declarations
-        // Boolean used for validation of string
-        // String of alphabet that will be incrementally stripped of letters found
         boolean isValid;
         String alph  = "abcdefghijklmnopqrstuvwxyz";
 
         // Quick check if empty string given, return whole alphabet
         if (input.length() == 0)
-            return  alph;
+            return alph;
 
-        // Replaces all spaces with empty space and sets it to lower case
         input = input.replaceAll(" ", "");
         input = input.toLowerCase();
 
-        // Converting input array to char array for ease of looping
         char[] charArray = input.toCharArray();
 
         for (char c: charArray) {
-            // If the array contains a letter between a-z AND 'alph' contains that character
-            // isValid is true, otherwise, false
             isValid = (((c >= 'a') && (c <= 'z')) && alph.contains(String.valueOf(c)));
 
-            // If isValid is true, replace the character with a blank space to signify that
-            // the letter has appeared and no longer needs to be included in the final output
             if (isValid)
                 alph = alph.replaceAll(String.valueOf(c), "");
 
-            // If length of alph goes to 0, all letter found.
-            // There is no need to continue the loop, so break and return alph (which is a blank string)
             if (alph.length() == 0)
                 break;
         }
@@ -38,6 +36,7 @@ public class ProblemOne {
         return alph;
     }
 
+    // Main method
     public static void main(String[] args) {
         System.out.println(getMissingLetters("A quick brown fox jumps over the lazy dog"));
         System.out.println(getMissingLetters("A slow yellow fox crawls under the proactive dog"));
--- src/ProblemTwo.java
+++ src/ProblemTwo.java
@@ -3,18 +3,21 @@ import java.util.List;
 
 public class ProblemTwo {
 
+    // Particle Class used to hold data for each particle
     public static class Particle {
 
-        private char direction;
-        private int speed, position;
-        private boolean exited;
+        private char direction; // Direction particle is moving, either 'L' or 'R'
+        private int speed, position; // Speed and initial position variables given in the object construction
+        private boolean exited; // If the Particle has exceeded the bounds of the array aka 'exited'
 
+        // Constructor which gives the particle its direction, speed, and position
         public Particle(char direction, int speed, int position) {
             this.direction = direction;
             this.speed = speed;
             this.position = position;
         }
 
+        // Moves the particle according to its speed
         public void movePosition() {
             if (this.direction == 'L')
                 this.position -= speed;
@@ -22,32 +25,28 @@ public class ProblemTwo {
                 this.position += speed;
         }
 
-        public void printPosition(int ceiling){
-            String outputLine = "";
-            System.out.println("Position of Particle " + direction + " is " + position);
-
-            for (int i = 0; i < ceiling; i++) {
-                if (position != i)
-                    outputLine += '.';
-                else
-                    outputLine += 'X';
-            }
-
-            System.out.println(outputLine);
-        }
-
-
+        // Checks to see if particle has moved outside of the bounds of the array, if so return true
         public boolean positionCheck(int ceiling) {
             exited = (this.position > ceiling || this.position < 0);
             return exited;
         }
 
+        // Gets position variable for checking purposes
         public int getPosition(){
             return position;
         }
 
     }
 
+    /*  Printing Method
+        - Method takes in List of particles and the ceiling of the original String.
+        - Iterates through going from 0 to the ceiling and for each Particle
+          the program will check to see if the current position of 'i' matches
+          the position of any particle, if so append 'X' to outputLine, break out of loop
+          and switch 'particleFound' to true.
+        - If a particle has not been found at that position the outputLine then appends '.' and continues on.
+        - Returns outputLine as a String.
+     */
     public static String printParticles(int ceiling, List<Particle> pArray) {
         StringBuilder outputLine = new StringBuilder();
         boolean particleFound = false;
@@ -69,55 +68,100 @@ public class ProblemTwo {
        return outputLine.toString();
     }
 
-    public static String[] animate(int speed, String init) {
-        char[] charArray = init.toCharArray();
+    /* Animate Function
+        - Creates two lists, one to hold all particles and one to hold all animation Strings.
+        - Creates a char array of the init String to better loop through.
+        - Creates an index variable to give to the objects of the Particle Class.
+        - Iterates through each char in charArray and if a 'L' or 'R' is found save its index
+          and create a new elements of the Particle list at that index.
+        - Then move into a while loop that goes until broken out of
+           - In while loop, iterate from size of pArray down to 0
+           - Move the position of each Particle and check to see if the position is out of bounds
+             if so, remove Particle from List.
+           - If pArray is now empty then break out of while loop otherwise, add new entry to animation
+             List and continue through while loop.
+     */
+    public static List<String> animate(int speed, String init) {
         List<Particle> pArray = new ArrayList<Particle>();
-        String[] finalAnimation = {""};
-        boolean notCompleted = true;
+        List<String> finalAnimation = new ArrayList<String>();
+        char[] charArray = init.toCharArray();
         int index;
 
         for (char c: charArray) {
             if (c == 'R' || c == 'L') {
-                index = new String(charArray).indexOf(c);
+                index = new String(charArray).indexOf(c); // Converts character place into an index
                 pArray.add(new Particle(c, speed, index));
-                charArray[index] = 'X';
+                charArray[index] = 'X'; // Sets found 'L' or 'R' index to X so that program wont get stuck on same char
             }
         }
 
-        System.out.println(printParticles(init.length(), pArray));
+        finalAnimation.add(printParticles(init.length(), pArray));
+        //System.out.println(printParticles(init.length(), pArray));
+        // ^ Above line used for debugging purposes
 
-        while(notCompleted) {
+        while(true) {
             for (int i = pArray.size() - 1; i >= 0; i--) {
-                //pArray.get(i).printPosition(init.length());
                 pArray.get(i).movePosition();
                 if (pArray.get(i).positionCheck(init.length())) {
                     pArray.remove(i);
                 }
             }
-            System.out.println(printParticles(init.length(), pArray));
 
             if (pArray.isEmpty()){
-                notCompleted = false;
+                break;
             }
 
+            finalAnimation.add(printParticles(init.length(), pArray));
+            //System.out.println(printParticles(init.length(), pArray));
+            // ^ Above line used for debugging purposes
         }
 
         return finalAnimation;
     }
 
-
+    // Main function
     public static void main(String[] args) {
-        animate(3, "RR..LRL");
-        System.out.println();
-        animate(2, "..R....");
-        System.out.println();
-        animate(2, "LRLR.LRLR");
-        System.out.println();
-        animate(10, "RLRLRLRLRL");
-        System.out.println();
-        animate(1, "...");
-        System.out.println();
-        animate(1, "LRRL.LR.LRR.R.LRRL.");
-        System.out.println();
+        long startTime = System.nanoTime();
+        System.out.println(animate(3, "RR..LRL"));
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        System.out.println("Duration to complete - " + duration);
+
+        startTime = System.nanoTime();
+        System.out.println(animate(2, "..R...."));
+        endTime = System.nanoTime();
+        duration = (endTime - startTime);
+        System.out.println("Duration to complete - " + duration);
+
+        startTime = System.nanoTime();
+        System.out.println(animate(2, "LRLR.LRLR"));
+        endTime = System.nanoTime();
+        duration = (endTime - startTime);
+        System.out.println("Duration to complete - " + duration);
+
+        startTime = System.nanoTime();
+        System.out.println(animate(10, "RLRLRLRLRL"));
+        endTime = System.nanoTime();
+        duration = (endTime - startTime);
+        System.out.println("Duration to complete - " + duration);
+
+        startTime = System.nanoTime();
+        System.out.println(animate(1, "..."));
+        endTime = System.nanoTime();
+        duration = (endTime - startTime);
+        System.out.println("Duration to complete - " + duration);
+
+        startTime = System.nanoTime();
+        System.out.println(animate(1, "LRRL.LR.LRR.R.LRRL."));
+        endTime = System.nanoTime();
+        duration = (endTime - startTime);
+        System.out.println("Duration to complete - " + duration);
+
+        startTime = System.nanoTime();
+        System.out.println(animate(1, "LRRL.LR.LRR.R.LRRL.LRRL.LR.LRR.R.LRRL.LRRL.LR.LRR.R.LRRL."));
+        endTime = System.nanoTime();
+        duration = (endTime - startTime);
+        System.out.println("Duration to complete - " + duration);
+
     }
 }
